4ed2404c3a9317a1d5271cd3567d2dc393f0a3f1
Lucas Fernandes Aguiar <aguiarl@aljazeera.net>
2023-07-20 22:04:16 -0300

    Fetch data from NYT API

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import nytApi from "./api/nytApi";
+import { Book } from "./interfaces/book";
+import { BookList } from "./components/BookList";
+import { Loading } from "./components/Loading";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState<boolean>(false);
+  const [books, setBooks] = useState<Book[]>([]);
+
+  const fetchBestSellers = async () => {
+    if (!books.length) {
+      setLoading(true);
+      const booksLoaded = await nytApi.fetchBestSellersBooks();
+      setBooks(booksLoaded);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchBestSellers();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="site-title">
+        <h1>Best Seller Books from NYT</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="site-body">
+        {loading ? <Loading /> : <BookList books={books} />}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/api/nytApi.ts
@@ -0,0 +1,29 @@
+import { Book } from "../interfaces/book";
+
+const fetchBestSellersBooks = async (): Promise<Book[]> => {
+  // Check if books are on local storage
+  const localStorageBooks = localStorage.getItem("books");
+  if (localStorageBooks) {
+    return JSON.parse(localStorageBooks);
+  }
+
+  // Fetch from NYT API
+  const API_URL = import.meta.env.VITE_NYT_API_URL;
+  const API_KEY = import.meta.env.VITE_NYT_API_KEY;
+  const response = await fetch(
+    `${API_URL}/svc/books/v3/lists/best-sellers/history.json?api-key=${API_KEY}`
+  );
+  if (response.ok) {
+    const data = await response.json();
+    const books: Book[] = data.results;
+    localStorage.setItem("books", JSON.stringify(books));
+    return books;
+  }
+  return [];
+};
+
+const nytApi = {
+  fetchBestSellersBooks,
+};
+
+export default nytApi;
